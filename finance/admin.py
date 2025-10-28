180a36b0fd85963f5c47f9f3e4d2a956412729e4
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-29 03:42:41 +0800

    ADD: Register Transaction and Category models in the admin site

--- finance/admin.py
+++ finance/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Transaction, Category
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Transaction)
+admin.site.register(Category)
