57643ffeef434d4e7dab05e6dcbcc7d5eeb467b0
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-29 03:09:09 +0800

    ADD: Implement transaction_list view to display transactions in descending order by date

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Transaction
 
 # Create your views here.
+def transaction_list(request):
+    transactions = Transaction.objects.all().order_by('-date')
+    return render(request, 'finance/transaction_list.html', {'transactions': transactions}) 
