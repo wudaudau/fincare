1f4a040b2ebca67d6217ec06eb0c8b1da9fd95d6
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-30 00:48:18 +0800

    ADD: Add login and logout views to URL patterns

--- fincare/urls.py
+++ fincare/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='finance/welcome.html'), name='login'),  # Login view
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout view
     path('finance/', include('finance.urls')),
 ]
