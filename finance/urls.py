aedf113c0a5818807c8e653ff2e271751ef3c3e6
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-30 01:33:26 +0800

    ADD: Add menu page URL to urlpatterns

--- finance/urls.py
+++ finance/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),  # welcome page
+    path('menu/', views.menu, name='menu'),  # menu page
     # TODO: path('dashboard/', views.dashboard, name='dashboard'),
     # TODO: path('reports/', views.reports, name='reports'),
     path('transaction_list/', views.transaction_list, name='transaction_list'),
