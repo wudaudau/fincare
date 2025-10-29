be453de6337592836f076ce71d0e355124541863
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-30 00:39:36 +0800

    ADD: Implement welcome view with authentication form

--- finance/views.py
+++ finance/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Transaction
 
 # Create your views here.
+def welcome(request):
+    form = AuthenticationForm()
+    return render(request, 'finance/welcome.html', {'form': form})
+
+
 def transaction_list(request):
     transactions = Transaction.objects.all().order_by('-date')
     return render(request, 'finance/transaction_list.html', {'transactions': transactions}) 
