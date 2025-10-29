d8dc052a673b5072fe6c91337441a221f0c5594b
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2025-10-30 00:44:54 +0800

    ADD: Add welcome page URL to urlpatterns

--- finance/urls.py
+++ finance/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.welcome, name='welcome'),  # welcome page
     # TODO: path('dashboard/', views.dashboard, name='dashboard'),
     # TODO: path('reports/', views.reports, name='reports'),
     path('transaction_list/', views.transaction_list, name='transaction_list'),
